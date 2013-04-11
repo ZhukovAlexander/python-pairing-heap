4bcca53996bf7063cdbb96c524b7f7efc77b5c59
Alexander Zhukov <zhukovaa90@gmail.com>
2013-04-11 18:23:25 +0300

    test_time garbage deletion

--- test_time.py
+++ test_time.py
@@ -17,33 +17,3 @@ def run(data):
     return sort == sorted(data)
 run(data)
 #cProfile.run('run(data)')
-def test():
-    s = set(range(10**4))
-    t1= time.clock()
-    while s:
-        s.pop()
-    t2= time.clock()
-    l = range(10**4)
-    t3= time.clock()
-    while l:
-        l.pop()
-    t4= time.clock()
-    print t2-t1, t4-t3
-test()
-def dummy():
-    pass
-class foo:
-    def dummy(self):
-        pass
-
-def u():
-    f = foo()
-    t1 = time.clock()
-    for i in range(10000):
-        dummy()
-    t2 = time.clock()
-    for i in range(10000):
-        f.dummy()
-    t3 = time.clock()
-    print t2-t1, t3-t2
-u()
